5d6e0ceed5f6744e94ae558d3ad33221712fde09
tenntenn6 <jbbcce@gmail.com>
2023-11-11 17:09:02 +0900

    ルーティング・コントローラー設置

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,6 @@
+class CompaniesController < ApplicationController
+  def show
+    @company_name = current_company.company_name
+    @spas = current_company.spas
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :companies
   root to: 'spas#index'
   resources :spas, only: [:index]
+  resources :companies, only: [:show]
   
 end
