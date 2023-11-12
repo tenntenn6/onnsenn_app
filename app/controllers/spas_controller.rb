f0d45c1e1a9e51f1874c478cea6aca86f182e5c9
tenntenn6 <jbbcce@gmail.com>
2023-11-12 16:08:44 +0900

    ルーティング・コントローラー・ストロングパロメーターの設置

--- app/controllers/spas_controller.rb
+++ app/controllers/spas_controller.rb
@@ -2,5 +2,20 @@ class SpasController < ApplicationController
   def index
     @spas = Spa.all 
   end
+
+  def new
+    @spa = Spa.new
+  end
+
+  def create
+    Spa.create(spa_params)
+    redirect_to '/'
+  end
+
+
+  private
+  def spa_params
+    params.require(:spa).permit(:name, :image, :detail, :facility, :business_hours, :access)
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :companies
   root to: 'spas#index'
-  resources :spas, only: [:index]
+  resources :spas, only: [:index, :new, :create]
   
 end
