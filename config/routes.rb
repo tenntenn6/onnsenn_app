e5160ad1af2dd7a6038d484d57d0ea21125545f0
tenntenn6 <jbbcce@gmail.com>
2023-11-15 15:03:12 +0900

    ルーティング設置

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :companies
   root to: 'spas#index'
   resources :spas
+    collection do
+      get 'search'
+    end
   resources :companies, only: [:show]
 
   
